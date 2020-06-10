cc63fcff0b32f73d7f8244e0033fa026c08bcb61
Allen Zhang <1124796703@qq.com>
2020-06-10 01:30:31 -0700

    No idea why but it's causing errors

--- app.js
+++ app.js
@@ -25,22 +25,6 @@ app.use('/mc-status', mc_status_router);
 app.use('/aeries', aeries);
 app.use("/", index);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
 app.listen(8002);
 console.log("Started listening on port 8002");
 
